a8f4554d10c7c8b8e714f933ee84f3b51814e646
ollieh-m <ollie.hm@cantab.net>
2016-04-12 11:53:42 +0100

    Challenge 10 - docking station releases a bike and bike works

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,8 +1,18 @@
 require 'DockingStation'
 
 describe DockingStation do
+
     it 'responds to release_bike' do
         expect(DockingStation.new).to respond_to 'release_bike'
     end
+
+    it 'should actually release a new bike - create a bike object - when release_bike is run' do
+    	expect(DockingStation.new.release_bike.class).to eq Bike
+    end
+
+    it 'should expect the bike to be working' do
+    	expect(DockingStation.new.release_bike.working?).to eq true
+    end
+
 end
  
