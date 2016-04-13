c5491507197566fbd11024de915d784bd6abe350
letianw <tmac4ever0824@gmail.com>
2016-04-13 23:08:48 +1000

    Lunch break

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,17 @@
 class Bike
+  def initialize
+    @broken = false
+  end
+
 	def working?
-		true
+		!broken?
 	end
+
+  def report_broken
+    @broken = true
+  end
+
+  def broken?
+    @broken
+  end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,25 @@
 require 'bike'
 
 describe Bike do
+
 	it 'should respond to working?' do
     is_expected.to respond_to('working?')
 	end
+
+  context 'When creating a new bike' do
+
+    it 'should respond to report_broken' do
+      is_expected.to respond_to('report_broken')
+    end
+
+    it 'after being reported as broken, it should be broken' do
+      subject.report_broken
+      expect(subject.broken?).to eq true
+    end
+
+    it 'after being reported as broken, it should cease to be working' do
+      subject.report_broken
+      expect(subject.working?).to eq false
+    end
+  end
 end
